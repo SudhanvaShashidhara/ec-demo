c1bf4d0f15b07d633436e9b35d96c2aad9ddf5af
Sudhanva Shashidhara <sudhanvashashidhara@gmail.com>
2022-05-05 03:26:07 +0530

    gtm fix maybe

--- pages/_document.js
+++ pages/_document.js
@@ -5,19 +5,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <Script
-          id="gtm-script"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-PCXRF7J');
-  `,
-          }}
-        />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
       <body>
@@ -31,6 +18,17 @@ export default function Document() {
         </noscript>
         <Main />
         <NextScript />
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PCXRF7J');`,
+          }}
+        />
       </body>
     </Html>
   );
