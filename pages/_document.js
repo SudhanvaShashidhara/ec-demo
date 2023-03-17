a2db7fe4b3de404c3f1e9198895a4131a23b6ad4
Sudhanva Shashidhara <sudhanvashashidhara@Sudhanvas-MacBook-Air.local>
2023-03-17 19:00:05 +0530

    Add EC Google Tag code for form page

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,19 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-PCXRF7J');`,
           }}
         ></script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=GT-KFNPS8T"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'GT-KFNPS8T');`,
+          }}
+        ></script>
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
       <body>
@@ -27,17 +40,33 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         </noscript>
         <Main />
         <NextScript />
-        {/* <Script
-          id="gtm-script"
-          strategy="afterInteractive"
+        <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-PCXRF7J');`,
+            __html: `
+      (function(){
+        var listen = false;
+        function listen_to_form_submit(){
+          if(!listen){
+            var timer = setInterval(function(){
+              if(document.querySelector('.form-submit-success')){
+                gtag('set', 'user_data', {
+                  "email": document.querySelector('.ec-form-email') ? document.querySelector('.ec-form-email').innerText.trim() : ''
+                });
+                gtag('event', 'conversion', {'send_to': 'AW-863193958/GRKUCPTnyZIYEOaWzZsD'});
+                clearInterval(timer);
+              }
+            }, 1000);
+          }
+        }
+        document.addEventListener('click', function(e){
+          if(e.target.closest('button[type="submit"]') && window.location.pathname.includes('/form')){
+            listen_to_form_submit();
+          }
+        });
+      })();
+      `,
           }}
-        /> */}
+        ></script>
       </body>
     </Html>
   );
